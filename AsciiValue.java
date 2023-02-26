0712f732974b79ae6d1bb52fb37ecdf828c55432
naveen <90080476+20AG1A0516@users.noreply.github.com>
2023-02-26 10:46:34 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,8 @@
+public class AsciiValue  
+{  
+public static void main(String[] String)   
+{  
+int ch1 = 'u';  
+System.out.println(ch1);  
+}  
+}  
---
+++ Product.java
@@ -0,0 +1,7 @@
+class Product{
+    public static void main(String[] args) {
+        int x=2,y=4;
+        int product=x*y;
+        System.out.print(product);
+    }
+}
